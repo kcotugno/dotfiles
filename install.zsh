e6767bd12a1d12761f79aa2ec182cdea21a3d152
Kevin Cotugno <kevin@kevincotugno.com>
2018-06-23 10:17:02 -0700

    Add vim to install

--- install.zsh
+++ install.zsh
@@ -9,12 +9,14 @@ function uninstall {
 	eval $rm "$HOME/.zshrc"
 	eval $rm "$HOME/.emacs.d"
 	eval $rm "$HOME/.tmux.conf"
+	eval $rm "$HOME/.config/nvim"
 }
 
 function install {
 	eval $link ".emacs.d" "$HOME/.emacs.d"
 	eval $link ".zshrc" "$HOME/.zshrc"
 	eval $link ".tmux.conf" "$HOME/.tmux.conf"
+	eval $link ".config/nvim" "$HOME/.config/nvim"
 }
 
 if [[ "$1" == "uninstall" ]]; then
