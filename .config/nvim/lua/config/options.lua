7b44339742a066ed62cb14033b2ab0ac7385f8b5
Kevin Cotugno <kevin@cotugno.us>
2023-10-06 06:13:04 -0700

    Replace nvim config with LazyVim

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,13 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+local opt = vim.opt
+
+opt.colorcolumn = "80,100"
+opt.cursorline = true
+opt.list = true
+opt.listchars = "tab:――,space:·,trail:·"
+opt.mouse = "a"
+opt.spell = true
+opt.termguicolors = true
+opt.wrap = false
