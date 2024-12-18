0fc3c8f504d5387d37d827d4f5a76207cabb1c3e
Kevin Cotugno <kevin@cotugno.family>
2024-12-18 06:28:50 -0700

    Replace telescope with fzf-lua

---
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,6 @@
+return {
+  "ibhagwan/fzf-lua",
+  keys = {
+    { "<c-p>", LazyVim.pick("files"), desc = "Find Files (Root Dir)", remap = true },
+  },
+}
--- .config/nvim/lua/plugins/telescope.lua
+++
@@ -1,8 +0,0 @@
-local Telescope = require("telescope.builtin")
-
-return {
-  "nvim-telescope/telescope.nvim",
-  keys = {
-    { "<c-p>", Telescope.find_files, desc = "Find Files (root dir)" },
-  },
-}
