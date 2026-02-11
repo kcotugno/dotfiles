472c0ccf0aa0d8c2010bbba29cf34a7ac2ee8cff
Kevin Cotugno <kevin@cotugno.us>
2026-02-11 10:32:01 -0700

    Use opt_local for go file formatting

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -5,8 +5,8 @@
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "go",
   callback = function(event)
-    vim.opt.expandtab = false
-    vim.opt.shiftwidth = 8
-    vim.opt.tabstop = 8
+    vim.opt_local.expandtab = false
+    vim.opt_local.shiftwidth = 8
+    vim.opt_local.tabstop = 8
   end,
 })
